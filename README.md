068072803b3a998801134fb8ef92e037c94621bf
lizunlong <lizunlong@gmail.com>
2015-10-31 11:12:14 +0800

    updated piece profile intro

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This is a sub-project of [Piece](https://github.com/lzl/piece).
 
-With Piece Profile, visitors can read your pieces, no need to know your user ID or what's the meaning of URL, just like Twitter profile page or Wordpress blog page.
+With Piece Profile, visitors can read your pieces, no need to know your user ID or the meaning of URL, just like Twitter profile page or WordPress blog page.
--- ui/Navbar.jsx
+++ ui/Navbar.jsx
@@ -2,13 +2,13 @@ Navbar = React.createClass({
   render() {
     return (
       <div className="card">
-        <img className="card-img-top" src="/avatar.png" alt="profile avatar" />
+        <img className="card-img-top" src="/holder.svg" alt="profile avatar" />
         <div className="card-block">
-          <p className="card-text">Yes, I found my lexi.</p>
+          <p className="card-text">This is a sub-project of <a href="https://github.com/lzl/piece">Piece</a>.</p>
+          <p className="card-text">With Piece Profile, visitors can read your pieces, no need to know your user ID or the meaning of URL, just like Twitter profile page or WordPress blog page.</p>
         </div>
         <div className="card-block">
-          <a href="https://lizunlong.com/" className="card-link">Blog</a>
-          <a href="https://github.com/lzl" className="card-link">GitHub</a>
+          <a href="https://github.com/lzl/piece-profile" className="card-link">GitHub</a>
         </div>
       </div>
     );
