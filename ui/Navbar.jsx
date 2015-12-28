930cf56fd4e23e7ee89b4c32b12c573219b000c1
lizunlong <lizunlong@gmail.com>
2015-12-28 17:34:59 +0800

    easy customization

--- ui/Navbar.jsx
+++ ui/Navbar.jsx
@@ -1,4 +1,18 @@
 Navbar = React.createClass({
+  renderText() {
+    const text = Meteor.settings.public.profile.text;
+    return text.map((text, index) => {
+      return <p key={index} className="card-text">{text}</p>;
+    })
+  },
+
+  renderLink() {
+    const link = Meteor.settings.public.profile.link;
+    return link.map((link, index) => {
+      return <a key={index} href={link.url} className="card-link">{link.name}</a>;
+    })
+  },
+
   render() {
     const avatar = Meteor.settings.public.profile.avatar;
     return (
@@ -6,12 +20,11 @@ Navbar = React.createClass({
         <img className="card-img-top" src={avatar} alt="profile avatar" />
 
         <div className="card-block">
-          <p className="card-text">This is a sub-project of <a href="https://github.com/lzl/piece">Piece</a>.</p>
-          <p className="card-text">With Piece Profile, visitors can read your pieces, no need to know your user ID or the meaning of URL, just like Twitter profile page or WordPress blog page.</p>
+          {this.renderText()}
         </div>
 
         <div className="card-block">
-          <a href="https://github.com/lzl/piece-profile" className="card-link">GitHub</a>
+          {this.renderLink()}
         </div>
 
         <div className="card-footer">
